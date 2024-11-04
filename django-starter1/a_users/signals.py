ac0104fbbb406818d1bdafd7924ff73800a4535e
Sukardi <sukardi@karyabangsa.sch.id>
2024-11-04 19:51:46 +0700

    Add files via upload

---
+++ django-starter1/a_users/signals.py
@@ -0,0 +1,37 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save
+from django.contrib.auth.models import User
+from .models import Profile
+from allauth.account.models import EmailAddress
+
+
+@receiver(post_save, sender=User)
+def user_postsave(sender, instance, created, **kwargs):
+    user = instance
+    
+    # add profile if user is created
+    if created:
+        Profile.objects.create(
+            user=user,
+        )
+    else:
+        # update allauth email address
+        try:
+            email_address = EmailAddress.objects.get_primary(user)
+            if email_address.email != user.email:
+                email_address.email = user.email
+                email_address.verified = False
+                email_address.save()
+        except:
+            # if allaauth email address does not exist, create one
+            EmailAddress.objects.create(
+                user=user,
+                email=user.email,
+                verified=False,
+                primary=True
+            )
+            
+@receiver(pre_save, sender=User)
+def user_presave(sender, instance, **kwargs):
+    if instance.username:
+        instance.username = instance.username.lower()        
